a2d3342e35eed6375e31da1adfe876096b1b58d3
Haruna Kongiz <kongiraharuna@yahoo.com>
2026-05-19 11:44:12 +0530

    fix: load firebase credentials from env vars

--- src/services/email.services.js
+++ src/services/email.services.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.ALERT_EMAIL,
     pass: process.env.ALERT_EMAIL_PASSWORD
